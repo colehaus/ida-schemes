c41930e14d1cbfd546efec23fd51fdc03d2b97fc
Cole Haus <colehaus@cryptolab.net>
2019-11-27 16:40:41 -0800

    Split into three steps
    Also, add some Q and A newtypes

--- src/Main.hs
+++ src/Main.hs
@@ -3,50 +3,69 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TupleSections #-}
 
 module Main where
 
 import Control.Monad ((<=<), (>=>))
-import Data.Functor.Foldable (Base, Corecursive (embed), Fix (Fix), Recursive, para)
+import Data.Functor.Foldable (Base, Corecursive (embed), Fix (Fix), Recursive, cata, para)
 
 data IDA q a f = Answer q a | Decomp q [f]
   deriving (Foldable, Functor, Traversable)
 
+type IDATree q a = Fix (IDA q a)
+
+newtype Q = Q { unQ :: String }
+newtype A = A { unA :: String }
+
 main :: IO ()
 main =
   putStrLn "What's the overall question?" >>
-  getLine >>=
+  Q <$> getLine >>=
   ida >>=
-  putStrLn . ("The answer is: " <>)
+  putStrLn . ("The answer is: " <>) . unA
 
-ida :: String -> IO String
-ida = paraM idaAlg <=< anaM idaCoalg
+ida :: Q -> IO A
+ida = aggregate <=< traverse' answer <=< decompose
 
-idaAlg :: IDA String String (Fix (IDA String String), String) -> IO String
-idaAlg (Answer _ a) = pure a
-idaAlg (Decomp q' qs') =
+decompose :: Q -> IO (IDATree Q a)
+decompose = anaM coalg
+
+aggregate :: IDATree Q A -> IO A
+aggregate = paraM alg
+
+answer :: IDA Q A f -> IO (IDA Q A f)
+answer (Decomp (Q q) []) = putStrLn "What's the answer?" >> putStrLn q >> (Answer (Q q) . A <$> getLine)
+answer x = pure x
+
+alg :: IDA Q A (Fix (IDA Q A), A) -> IO A
+alg (Answer _ a) = pure a
+alg (Decomp (Q q') qs') =
   putStrLn ("Original question: " <> q') >>
   putStrLn "Subquestions and subanswers:" >>
   traverse display qs' >>
   putStrLn "What's the answer?" >>
-  getLine
+  A <$> getLine
   where
-    display (Fix (Decomp q _), a) = putStrLn ("Q: " <> q <> "\nA: " <> a)
-    display (Fix (Answer q _), a) = putStrLn ("Q: " <> q <> "\nA: " <> a)
+    display (Fix (Decomp (Q q) _), A a) = putStrLn ("Q: " <> q <> "\nA: " <> a)
+    display (Fix (Answer (Q q) _), A a) = putStrLn ("Q: " <> q <> "\nA: " <> a)
 
-idaCoalg :: String -> IO (IDA String String String)
-idaCoalg q' =
+traverse' :: Monad m => (forall f. IDA q a f -> m (IDA q' a' f)) -> Fix (IDA q a) -> m (Fix (IDA q' a'))
+traverse' f = cataM (fmap Fix . f)
+
+coalg :: Q -> IO (IDA Q a Q)
+coalg (Q q') =
   putStrLn ("Q: " <> q') >>
   inquire "Can you immediately answer this question?" >>=
     \case
-      True -> putStrLn "What's the answer?" >> (Answer q' <$> getLine)
-      False -> Decomp q' <$> subqs First
+      True -> pure $ Decomp (Q q') []
+      False -> Decomp (Q q') <$> subqs First
   where
     recurse s = do
       putStrLn s
       q <- getLine
-      (q :) <$> subqs Not
+      (Q q :) <$> subqs Not
     subqs Not =
       inquire "Any more subquestions?" >>=
         \case
@@ -62,6 +81,9 @@ inquire s = do
   putStrLn (s <> " y/n")
   res <- getLine
   pure $ res == "y" || res == "Y"
+
+cataM :: (Traversable (Base t), Monad m, Recursive t) => (Base t c -> m c) -> t -> m c
+cataM = cata . (sequence >=>)
 
 paraM :: (Traversable (Base t), Monad m, Recursive t) => (Base t (t, c) -> m c) -> t -> m c
 paraM f = para (sequence . fmap pack >=> f)
